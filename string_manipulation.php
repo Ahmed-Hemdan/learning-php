005a15affc6877c0cf8d8d7a5649391952695f02
Hemdan <ahmedhemdan.dev@gmail.com>
2026-05-03 16:21:09 +0300

    take a look

--- string_manipulation.php
+++ string_manipulation.php
@@ -3,33 +3,29 @@
 
 $hello = "hello every body";
 
-echo strlen($hello)."<br>";
-echo strpos($hello , "l")."<br>";
-echo strpos($hello , "e" ,5)."<br>";
-echo strtoupper($hello)."<br>";
-echo strtolower($hello)."<br>";
-echo str_replace("hello","hi", $hello)."<br>";
-echo str_repeat($hello."<br>",5)."<br>";
-echo ucfirst($hello)."<br>";
-echo lcfirst($hello)."<br>";
-echo str_word_count($hello)."<br>";
-echo strrev($hello)."<br>";
+echo substr($hello,0,5)."<br/>"; // this function to get part of string
+echo strpos($hello , "llo")."<br/>"; // this function to get the position of string in your string 
+echo strpos($hello , "e", 3)."<br/>"; // this function to get the position of string in your string but start search from position 3
+echo strlen($hello)."<br/>"; // this function to get the length of string
+echo ucfirst($hello)."<br/>"; // this function to make the first letter in string capital
+echo strtoupper($hello)."<br/>"; // this function to make all letters in string capital
+echo strtolower($hello)."<br/>"; // this function to make all letters in string lowercase
+echo str_replace("hello", "hi", $hello)."<br/>"; // this function to replace word in string with another word
+echo str_repeat($hello, 3)."<br/>"; // this function to repeat string a number of times
+echo str_shuffle($hello)."<br/>"; // this function to shuffle the letters in string تلغبطهم يعني :D
+echo str_word_count($hello)."<br/>"; // this function to count the number of words in string
+echo strrev($hello)."<br/>"; // this function to reverse the string
+echo "<br/><br/>";
 
 
-$name ="        Ahmed           ";
+$x = "        Hemdan        ";
+echo trim($x)."<br/>"; // this function to remove spaces from the beginning and end of string
 
-echo strlen($name)."<br>";
-echo trim($name)."<br>";
-
-    $name = "<b>Ahmed</b>";
-    echo $name."<br>";
-    echo strip_tags($name)."<br>";
-
-$name = strip_tags($name);
-
-$y = substr($hello,0,5).$name; 
-
-echo $y."<br>";
+$y ="<b>hemdan</b>";
+echo strip_tags($y)."<br/>"; // this function to remove html tags from string
+echo strlen($y)."<br/>"; // this function to get the length of string with html tags
+echo strlen(strip_tags($y))."<br/>"; // this function to get the length of string without html tags
+echo htmlspecialchars($y)."<br/>"; // this function to convert html tags to html entities يعني هيظهر التاجات ككلمات مش كود
 
 // there is a lot more you can try it with yourself <3
 
