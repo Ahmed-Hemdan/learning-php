a50ada0c7640bda5380799113525b64f1f94adc5
Hemdan <ahmedhemdan.dev@gmail.com>
2026-05-05 09:45:08 +0300

    super global

---
+++ super_globals.php
@@ -0,0 +1,47 @@
+<?php
+    //super globals 
+
+    $x= 5; 
+     function test(){
+        echo $GLOBALS['x']."<br>";
+     }
+
+     test();
+
+
+     // $_server
+
+     echo $_SERVER['PHP_SELF'] ."<br>";
+     
+     echo $_SERVER["SERVER_NAME"]."<br>";
+
+     echo $_SESRVER["HTTP_HOST"]."<br>";
+
+    echo $_SERVER["HTTP_USER_AGENT"]."<br>";
+
+    // print_r($_SERVER)."<br>";
+     
+    echo $_SERVER["SCRIPT_NAME"]."<br>";
+
+    echo $_SERVER["REMOTE_ADDR"]."<br>";
+
+    echo $_SERVER['REQUEST_METHOD']."<br>";
+
+
+    // $_REQUEST
+
+
+    echo $_REQUEST["m"]."<br>";
+?>
+
+<html>
+
+<head></head>   
+<body>
+    
+<form method="post">
+    <input name="m">
+    <input type="submit">
+</form>
+</body>
+</html>
