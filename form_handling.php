90adea589964cab19f1d9afd6aff963a5610151b
Hemdan <ahmedhemdan.dev@gmail.com>
2026-05-14 08:45:50 +0300

    validate on all form and hashing

--- form_handling.php
+++ form_handling.php
@@ -90,37 +90,101 @@
 
 // }
 
-if(isset($_POST['show'])){
-    if(isset($_FILES['image'])){
-    print_r($_FILES['image']);
+// if(isset($_POST['show'])){
+//     if(isset($_FILES['image'])){
+//     print_r($_FILES['image']);
 
-    $file_name = $_FILES['image']["name"];
-    $file_size= $_FILES['image']['size'];
-    $file_tpm = $_FILES['image']['tmp_name'];
-    $file_type= $_FILES['image']['type'];
+//     $file_name = $_FILES['image']["name"];
+//     $file_size= $_FILES['image']['size'];
+//     $file_tpm = $_FILES['image']['tmp_name'];
+//     $file_type= $_FILES['image']['type'];
     
-    $avalibale_ext= array("jpg","png",jpeg);
-    //strtolower this to take care of char senc 
-    // end to just to take the end of the sentance after /
-    // explode to to cut the file type from / mark 
-    $ext= strtolower(end(explode("/",$file_type)));
+//     $avalibale_ext= array("jpg","png",jpeg);
+//     //strtolower this to take care of char senc 
+//     // end to just to take the end of the sentance after /
+//     // explode to to cut the file type from / mark 
+//     $ext= strtolower(end(explode("/",$file_type)));
 
-    echo $ext ."<br>";
+//     echo $ext ."<br>";
 
-    if(in_array($ext,$avalibale_ext)){
-        echo "valid ext <br>";
-    }else{
-        echo "invalid ext <br>";
-    }
+//     if(in_array($ext,$avalibale_ext)){
+//         echo "valid ext <br>";
+//     }else{
+//         echo "invalid ext <br>";
+//     }
 
-        if(!$error){
-            move_uploaded_file($file_tpm,"images/".$file_name );
-        }else{
-            echo "Error <br>";
-        }
+//         if(!$error){
+//             move_uploaded_file($file_tpm,"images/".$file_name );
+//         }else{
+//             echo "Error <br>";
+//         }
 
 
-}
+// }
+// }
+
+
+if(isset($_POST['show'])){
+            $fname= check($_POST['fname']);
+            $lname= check($_POST['lname']);
+            $pass= check($_POST['pass']);
+            $phone=check($_POST['phone']);
+            $email= check($_POST['email']);
+            $website ="http://".check($_POST['website']);
+            $comment =check($_POST['comment']);
+            if(!$_POST['gender']) {
+                $gender = 'male';
+                echo $gender."<br>";
+            }else{
+                $gender = $_POST['gender']  ;
+                echo $gender."<br>";
+            }
+            // first name required - alpha - < 15 char
+            // last name required - alpha - < 15 char
+            // username required -numbers , alpha < 15 char
+            // pass required  - numbers , alpha , symbols <=6 , hashing  
+            // phone number - numbers , required , 11 char 
+            // email email , required 
+            // link url , required 
+            // comment 
+
+
+            if(!$fname || !ctype_alpha($fname) || strlen($fname)>15 ){
+               echo "invalid first name<br>";
+            }
+            if(!$lname || !ctype_alpha($lname) || strlen($lname)>15 ){
+               echo "invalid last name <br>";
+            }
+            if(!$pass || strlen($pass)<6 ){
+              echo 'invalid pass<br>';
+            }else{
+                $pass = md5($pass);
+                echo $pass."<br>";
+            }
+            if(!$phone || !ctype_digit($phone) || strlen($phone)!= 11){
+                echo "invalid phone number<br>";
+            }
+            if(!$email || !filter_var($email , FILTER_VALIDATE_EMAIL)){
+                echo "invalid email <br>";
+            }
+            if(!$website || !filter_var($website, FILTER_VALIDATE_URL)){
+                echo "invalid website <br>";    
+            }
+            if(isset($_FILES['image'])){
+                $img_name= $_FILES['image']["name"];
+                $img_size= $_FILES['image']['size'];
+                $img_tmp=$_FILES['image']['tmp_name'];
+                $img_ext=strtolower(end(explode(".", $img_name)))   ;
+                print_r($_FILES['image']);
+                $avl_ext = array("jpg","png");
+                if($img_size > 2097152 || !in_array($img_ext , $avl_ext)) {
+                    echo "uploade image error <br>";
+                }else if(!$img_name){
+                    echo "uploade your image<br> "      ;
+                }else{
+                    move_uploaded_file($img_tmp , "images/".$img_name);
+                }
+            }
 }
 
 ?>
@@ -155,12 +219,12 @@ if(isset($_POST['show'])){
                         </tr>
                         <tr>
                             <td>  Website:</td>
-                            <td><input type="text" name="link"></td>
+                            <td><input type="text" name="website"></td>
                         </tr>
                         <tr>
                             <td>Gender:</td>
-                            <td><input type="radio" value="Male"  name="male"> Male </td>
-                            <td> <input type="radio" value="Female"  name="female"> Female</td>
+                            <td><input type="radio" value="Male"  name="gender"> Male </td>
+                            <td> <input type="radio" value="Female"  name="gender"> Female</td>
                         </tr>
                         <tr>
                             <td>  Image:</td>
