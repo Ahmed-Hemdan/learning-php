28cf317c80aaf8b1a51e495c636fa230ed26a6e6
Hemdan <ahmedhemdan.dev@gmail.com>
2026-05-16 11:05:36 +0300

    where in sql

--- get_data_from_db.php
+++ get_data_from_db.php
@@ -2,9 +2,10 @@
 require "connect_php_with_sql.php";
 //  $query = "SELECT `id` , `user_name` , `pass` FROM `accounts`"; 
 //  you can select what you want like this but if you want to get all fields you can shortcut with *
-    $query = "SELECT * FROM `accounts`";
+//  if you want to search in your db use where you can add multiable conditions
+    $query = "SELECT * FROM `accounts` WHERE `id`>5 AND `user_name`='hemdan' OR `user_name`= 'mohamed' ";    // it's not case sensitive 
     $result = mysqli_query($connection,$query);
-
+    // echo mysqli_num_rows($result);
     while($row=mysqli_fetch_array($result)){
         echo $row['id']."   ". $row['user_name']."   ".$row['pass']."<br>";
     }
